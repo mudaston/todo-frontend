e06993057c8b6b4532bab159518c35d9f601743f
mudaston <twil.revaas@gmail.com>
2023-07-11 13:47:12 +0300

    add ThemeSwitcher component to header and add styles to Typography

--- src/feature/header/header.tsx
+++ src/feature/header/header.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { AppBar, Typography, Toolbar } from '@mui/material'
 
+import { ThemeSwitcher } from '@feature/theme-switcher'
+
 import { styles } from './styles'
 
 interface HeaderProps {}
@@ -11,7 +13,10 @@ const Header: React.FC<Props> = () => {
     return (
         <AppBar position='sticky' sx={styles['header']}>
             <Toolbar>
-                <Typography variant='h6'>Mudastone</Typography>
+                <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+                    Mudastone
+                </Typography>
+                <ThemeSwitcher />
             </Toolbar>
         </AppBar>
     )
