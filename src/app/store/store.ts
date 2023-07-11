bb7fa94879565a6e3193eb53d8b1c875af790fd8
mudaston <twil.revaas@gmail.com>
2023-07-11 13:50:14 +0300

    configure store to persist theme reducer

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore } from 'redux-persist'
+import thunk from 'redux-thunk'
 
-import { testSlice } from './test-slice'
+import { reducers } from './reducers'
 
 export const store = configureStore({
-    reducer: {
-        [testSlice.name]: testSlice.reducer,
-    },
+    reducer: reducers,
     devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
